17d471541bab523defb71d85ca33ebc55fbd136f
jatin-js <you@example.co  git config --global user.name Your>
2020-01-17 20:20:52 +0530

    STL for

---
+++ STL/stlfor.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t, q;
+    cin >> t;
+    vector<int> v;
+    for (q = 0; q < t; q++)
+    {
+        vector<int>::iterator it;
+        for (it = v.begin(); it != v.end(); it++)
+        {
+        }
+
+        list<int>::iterator it;
+        for (it = l.start(); it != l.end(); it++)
+        {
+        }
+
+        priority_queue<int, vector<int>, greater<int>> minH;
+
+        map<int, int> m;
+        map<int, int>::iterator it;
+        for (it = m.begin(); it != m.end(); it++)
+        {
+        }
+
+        set<int> s;
+        set<int>::iterator it;
+        for (it = s.begin(); it != s.end(); it++)
+        {
+        }
+
+        return 0;
+    }
