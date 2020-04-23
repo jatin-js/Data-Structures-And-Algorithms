8549050491ae44314288603427057e67cbbbad78
jatin-js <jatinhmu@gmail.com>
2020-04-23 17:30:09 +0530

    cpp

---
+++ cpp/a.cpp
@@ -0,0 +1,41 @@
+/*
+* Note: This template uses some c++11 functions , so you have to compile it with c++11 flag.
+*       Example:-   $ g++ -std=c++11 a.cpp
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    for (char i : "ABCD")
+    {
+        cout << i;
+    }
+    cout << '\n';
+}
